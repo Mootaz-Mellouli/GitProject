6f54859f26d2da44217a686d84f2dbb53f7178b5
Saif-Eddine Hazemi <saif-eddine.hazemi@inetum.com>
2023-04-08 10:51:01 +0100

    Commit Saif

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,4 +1,13 @@
 package com.esprit.examen.controllers;
 
+@RestController
+@RequestMapping("/projet")
+@RequiredArgsConstructor
 public class ProjectController {
+    private final IFactureService factureService;
+
+    @GetMapping
+    public List<Facture> getFactures() {
+        return factureService.retrieveAllFactures();
+    }
 }
