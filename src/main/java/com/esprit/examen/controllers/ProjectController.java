e1eb5fb29069ea2ae88f3a3b862b329f13b9dc48
amani amdouni <amani.amdouni@esprit>
2023-04-08 12:04:08 +0200

    -a

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,4 +1,10 @@
 package com.esprit.examen.controllers;
 
 public class ProjectController {
+    
+	@PostMapping
+	public Projet  addProjet(@RequestBody Fournisseur f) {
+		Projet fournisseur = fournisseurService.addFournisseur(f);
+		return fournisseur;
+	}
 }
