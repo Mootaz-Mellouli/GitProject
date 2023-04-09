4bf23c5aebe154e09346856593551476fe4c648a
amaniamdouni <amdouniamani777@gmail.com>
2023-04-09 17:54:23 +0200

    second commit

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,10 +1,42 @@
 package com.esprit.examen.controllers;
 
-public class ProjectController {
-    
+@RestController
+@Api(tags = "Gestion des fournisseurss")
+@RequestMapping("/fournisseur")
+@RequiredArgsConstructor
+public class FournisseurRestController {
+
+	private final IFournisseurService fournisseurService;
+
+	@GetMapping
+	public List<Fournisseur> getFournisseurs() {
+		return fournisseurService.retrieveAllFournisseurs();
+	}
+
+	@GetMapping("/{fournisseur-id}")
+	public Fournisseur retrieveFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
+		return fournisseurService.retrieveFournisseur(fournisseurId);
+	}
+
 	@PostMapping
-	public Projet  addProjet(@RequestBody Fournisseur f) {
-		Projet fournisseur = fournisseurService.addFournisseur(f);
+	public Fournisseur addFournisseur(@RequestBody Fournisseur f) {
+		Fournisseur fournisseur = fournisseurService.addFournisseur(f);
 		return fournisseur;
 	}
+
+	@DeleteMapping("/{fournisseur-id}")
+	public void removeFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
+		fournisseurService.deleteFournisseur(fournisseurId);
+	}
+
+	@PutMapping
+	public Fournisseur modifyFournisseur(@RequestBody Fournisseur fournisseur) {
+		return fournisseurService.updateFournisseur(fournisseur);
+	}
+
+		@PutMapping(value = "/assignSecteurActiviteToFournisseur/{idSecteurActivite}/{idFournisseur}")
+		public void assignProduitToStock(@PathVariable("idSecteurActivite") Long idSecteurActivite, @PathVariable("idFournisseur") Long idFournisseur) {
+			fournisseurService.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur);
+		}
 }
+
