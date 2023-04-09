ba6b6f2a5ef4b4b64fc2dfce9c4a0c2bbb73a24f
farouksnoussi <farouksno@gmail.com>
2023-04-09 23:54:35 +0200

    add methode in controller

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,4 +1,18 @@
 package com.esprit.examen.controllers;
 
 public class ProjectController {
+    	@PostMapping
+	public Produit addProduit(@RequestBody Produit p) {
+		return produitService.addProduit(p);
+	}
+    
+    	@DeleteMapping("/{produit-id}")
+	public void removeProduit(@PathVariable("produit-id") Long produitId) {
+		produitService.deleteProduit(produitId);
+	}
+
+	@PutMapping
+	public Produit modifyProduit(@RequestBody Produit p) {
+		return produitService.updateProduit(p);
+	}
 }
