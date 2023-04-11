97871fd1ab2409ae32dec68415040efdbc706b05
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2023-04-11 21:57:48 +0100

    update controller

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,4 +1,16 @@
 package com.esprit.examen.controllers;
 
 public class ProjectController {
+
+    private final IStockService stockService;
+
+	@GetMapping("/retrieve-all-stocks")
+	public List<Stock> getStocks() {
+		return stockService.retrieveAllStocks();
+	}
+
+	@GetMapping("/{stock-id}")
+	public Stock retrieveStock(@PathVariable("stock-id") Long stockId) {
+		return stockService.retrieveStock(stockId);
+	}
 }
