b6862c71b917080934287b7ba847856cf03cb766
Saif-Eddine Hazemi <saif-eddine.hazemi@inetum.com>
2023-04-15 02:37:22 +0100

    Saif Commit 15/04/2023

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -10,4 +10,53 @@ public class ProjectController {
     public List<Facture> getFactures() {
         return factureService.retrieveAllFactures();
     }
+
+		public List<SecteurActivite> getSecteurActivite() {
+		return secteurActiviteService.retrieveAllSecteurActivite();
+	}
+
+	@GetMapping("/{secteurActivite-id}")
+	public SecteurActivite retrieveSecteurActivite(@PathVariable("secteurActivite-id") Long secteurActiviteId) {
+		return secteurActiviteService.retrieveSecteurActivite(secteurActiviteId);
+	}
+
+	@PostMapping
+	public SecteurActivite addSecteurActivite(@RequestBody SecteurActivite sa) {
+		SecteurActivite secteurActivite = secteurActiviteService.addSecteurActivite(sa);
+		return secteurActivite;
+	}
+
+	@DeleteMapping("/{secteurActivite-id}")
+	public void removeSecteurActivite(@PathVariable("secteurActivite-id") Long secteurActiviteId) {
+		secteurActiviteService.deleteSecteurActivite(secteurActiviteId);
+	}
+
+	@PutMapping
+	public SecteurActivite modifySecteurActivite(@RequestBody SecteurActivite secteurActivite) {
+		return secteurActiviteService.updateSecteurActivite(secteurActivite);
+	}
+		public List<SecteurActivite> getSecteurActivite() {
+		return secteurActiviteService.retrieveAllSecteurActivite();
+	}
+
+	@GetMapping("/{secteurActivite-id}")
+	public SecteurActivite retrieveSecteurActivite(@PathVariable("secteurActivite-id") Long secteurActiviteId) {
+		return secteurActiviteService.retrieveSecteurActivite(secteurActiviteId);
+	}
+
+	@PostMapping
+	public SecteurActivite addSecteurActivite(@RequestBody SecteurActivite sa) {
+		SecteurActivite secteurActivite = secteurActiviteService.addSecteurActivite(sa);
+		return secteurActivite;
+	}
+
+	@DeleteMapping("/{secteurActivite-id}")
+	public void removeSecteurActivite(@PathVariable("secteurActivite-id") Long secteurActiviteId) {
+		secteurActiviteService.deleteSecteurActivite(secteurActiviteId);
+	}
+
+	@PutMapping
+	public SecteurActivite modifySecteurActivite(@RequestBody SecteurActivite secteurActivite) {
+		return secteurActiviteService.updateSecteurActivite(secteurActivite);
+	}
 }
