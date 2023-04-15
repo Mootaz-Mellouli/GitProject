8643d1bd0f9e67aff05de87144e070290c2dd5f3
fai <farah.abidi@agena3000.com>
2023-04-15 03:24:12 +0200

    commmit main

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,6 +1,5 @@
 package com.esprit.examen.controllers;
 
-<<<<<<< HEAD
 @RestController
 @Api(tags = "Gestion des fournisseurss")
 @RequestMapping("/fournisseur")
@@ -39,44 +38,5 @@ public class FournisseurRestController {
 		public void assignProduitToStock(@PathVariable("idSecteurActivite") Long idSecteurActivite, @PathVariable("idFournisseur") Long idFournisseur) {
 			fournisseurService.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur);
 		}
-=======
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class ProjectController {
-	@Autowired
-    private final IStockService stockService;
-    
-    	@PostMapping
-	public Produit addProduit(@RequestBody Produit p) {
-		return produitService.addProduit(p);
-	}
-
-    	@DeleteMapping("/{produit-id}")
-	public void removeProduit(@PathVariable("produit-id") Long produitId) {
-		produitService.deleteProduit(produitId);
-	}
-
-	@PutMapping
-	public Produit modifyProduit(@RequestBody Produit p) {
-		return produitService.updateProduit(p);
-	}
-
-    	@PutMapping(value = "/assignProduitToStock/{idProduit}/{idStock}")
-	public void assignProduitToStock(@PathVariable("idProduit") Long idProduit, @PathVariable("idStock") Long idStock) {
-		produitService.assignProduitToStock(idProduit, idStock);
-	}
-
-	@GetMapping("/retrieve-all-stocks")
-	public List<Stock> getStocks() {
-		return stockService.retrieveAllStocks();
-	}
-
-	@GetMapping("/{stock-id}")
-	public Stock retrieveStock(@PathVariable("stock-id") Long stockId) {
-		return stockService.retrieveStock(stockId);
-	}
-	
-
->>>>>>> 87f39f679a19ab02a5fa52d022330baa9016f5b6
 }
 
